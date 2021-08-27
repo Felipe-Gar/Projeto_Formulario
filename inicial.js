529d3c2600f9c592282fe19233d0ef22661e1d31
FELIPE DA SILVA GARCIA <garcia.f@alunoescolassociais.org.br>
2021-08-27 16:55:25 -0300

    atualizaçao

---
+++ inicial.js
@@ -0,0 +1,12 @@
+function Acessar() {
+     var email = document.getElementById("email").value;
+
+     if(email ==""){
+         alert("Preencha todos os campos");
+
+     }else{
+         window.open("Cadastro.html");
+     }
+
+    
+}
